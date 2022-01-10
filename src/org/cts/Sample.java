604e9bddce490f480ac17d856adeb514fa65b9a8
git9897 <jalabulajangu2@gmail.com>
2022-01-10 10:56:14 +0530

    task done

--- src/org/cts/Sample.java
+++ src/org/cts/Sample.java
@@ -1,5 +1,11 @@
 package org.cts;
 
 public class Sample {
-
+	private void add() {
+		System.out.println("add");
+	}
+	public static void main(String[] args) {
+		Sample s = new Sample();
+		s.add();
+	}
 }
